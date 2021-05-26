79b5efada0ae934cc64487866e3f268a1530adec
Alexey Pryadka <todoaletodo@gmail.com>
2021-05-26 23:39:06 +0300

    commit

---
+++ application/config/db.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'host' => 'localhost',
+	'name' => 'mrcat',
+	'user' => 'root',
+	'password' => 'root',
+];
---
+++ application/views/admin/login.php
@@ -0,0 +1,21 @@
+<div class="container">
+  <div class="card card-login mx-auto mt-5">
+    <div class="card-header">Стань богом! Мяу!</div>
+    <div class="card-body">
+      <form action="/admin/login" method="post">
+        <div class="form-group">
+          <label>Логин</label>
+          <input class="form-control" type="text" name="login" />
+        </div>
+        <div class="form-group">
+          <label>Пароль</label>
+          <input class="form-control" type="password" name="password" />
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">Вход</button>
+      </form>
+    </div>
+  </div>
+  <p style="text-align: center; font-size: 16px; margin-top: 15px">
+    <a href="/">На главную</a>
+  </p>
+</div>
